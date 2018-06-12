69422b8e8057df7f6983fc530fac4801e05d9858
Ganesh Jangal <ganesh.jangal@outlook.com>
2018-06-11 21:55:05 -0400

    Fleshing out CashValueRepository class WIP

--- PortfolioTracker.HistoryService/PortfolioTracker.HistoryService/Repository/CashValueRepository.cs
+++ PortfolioTracker.HistoryService/PortfolioTracker.HistoryService/Repository/CashValueRepository.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using Serilog;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +16,102 @@ namespace PortfolioTracker.HistoryService.Repository
     }
     public class CashValueRepository : ICashValueRepository
     {
-        public Task<bool> DeleteAsync(int Id)
+        private readonly ILogger logger;
+        private readonly string connectionString;
+        public CashValueRepository(
+            ILogger logger)
         {
-            throw new NotImplementedException();
+            this.logger = logger;
+            this.connectionString = ConfigurationManager.AppSettings["HistoryDb"].ToString();
         }
 
-        public Task<IEnumerable<ICashValue>> GetListAsync()
+        public async Task<bool> DeleteAsync(int Id)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string sql = $"DELETE from dbo.CashValue where Id={Id}";
+
+                var rows = await connection.ExecuteAsync(sql);
+
+                if(rows > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
         }
 
-        public Task<ICashValue> GetSingleAsync(int Id)
+        public async Task<IEnumerable<ICashValue>> GetListAsync()
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT Id, PortfolioId, Cash, Date from dbo.CashValue";
+
+                return await connection.QueryAsync<ICashValue>(sql);
+            }
         }
 
-        public Task<bool> InsertAsync(ICashValue lot)
+        public async Task<ICashValue> GetSingleAsync(int Id)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT Id, PortfolioId, Cash, Date from dbo.CashValue where Id={Id}";
+
+                return (await connection.QueryAsync<ICashValue>(sql)).FirstOrDefault();
+            }
         }
 
-        public Task<bool> UpdateAysnc(ICashValue lot)
+        public async Task<bool> InsertAsync(ICashValue lot)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Check if exists else update
+                var selectSql = $@"SELECT count(*) from [dbo].[CashValue] WHERE PortfolioId={lot.PortfolioId} and Date='{lot.Date}'";
+
+                int rows = (int)(await connection.ExecuteScalarAsync(selectSql));
+
+                if (rows > 0)
+                {
+                    return await Update(lot, connection);
+                }
+
+                var sql = $"INSERT [dbo].[CashValue] ([PortfolioId],[Cash], [Date]) VALUES (@PortfolioId, @Cash, @Date )";
+                rows = await connection.ExecuteAsync(sql, new { lot.PortfolioId, lot.Cash, lot.Date });
+
+                if (rows > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAysnc(ICashValue lot)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                return await Update(lot, connection);
+            }
+        }
+
+        private static async Task<bool> Update(ICashValue lot, SqlConnection connection)
+        {
+            var sql = $@"UPDATE [dbo].[PortfolioValue] 
+                                SET [Value]={lot.Cash} 
+                            WHERE PortfolioId={lot.PortfolioId} and AsOf='{lot.Date}'";
+            int rows = await connection.ExecuteAsync(sql, new { lot.PortfolioId, lot.Cash, lot.Date });
+
+            if (rows > 0)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
